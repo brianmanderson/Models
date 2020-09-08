b880b46f2702765ee9963b7b9ed12044e06f8d08
markba122@gmail.com <bmanderson@mdanderson.org>
2020-09-08 16:51:37 -0500

    pull custom things form the Custom_Layers block

--- TF_Keras_Models.py
+++ TF_Keras_Models.py
@@ -6,23 +6,13 @@ import tensorflow.keras.backend as K
 from tensorflow.keras.layers import *
 from functools import partial, update_wrapper
 import tensorflow as tf
+import os, sys
+sys.path.append('.')
+from .Custom_Layers import *
 from tensorflow.python.keras.losses import LossFunctionWrapper, losses_utils, ops, math_ops, array_ops, smart_cond
 from tensorflow.python.keras.backend import nn, variables_module, variable, _constant_to_tensor, clip_ops, epsilon
 # SGD = tf.train.experimental.enable_mixed_precision_graph_rewrite(SGD())
 
-
-def ExpandDimension(axis):
-    import tensorflow.keras.backend as K
-    def expanddimension(x):
-        return K.expand_dims(x, axis)
-    return expanddimension
-
-
-def SqueezeDimension(axis):
-    import tensorflow.keras.backend as K
-    def squeezedimension(x):
-        return K.squeeze(x, axis)
-    return squeezedimension
 
 # x = layers.Lambda(SqueezeDimension(axis=0))(x)
 ExpandDimension_old = lambda axis: Lambda(lambda x: K.expand_dims(x, axis))
@@ -31,49 +21,7 @@ Subtract_new = lambda y: Lambda(lambda x: Subtract()([x, y]))
 Multipy_new = lambda y: Lambda(lambda x: Multiply()([x, y]))
 
 
-class WeightedCategoricalCrossentropy(LossFunctionWrapper):
-    def __init__(self, weights, from_logits=False, label_smoothing=0, reduction=losses_utils.ReductionV2.AUTO,
-                 name='weighted_categorical_crossentropy'):
-        self.weights = weights
-        weights = variable(weights)
 
-        def weighted_keras_categorical_crossentropy(target, output, from_logits=False, axis=-1):
-            target.shape.assert_is_compatible_with(output.shape)
-            if from_logits:
-                return nn.softmax_cross_entropy_with_logits_v2(labels=target, logits=output, axis=axis)
-            if not isinstance(output, (ops.EagerTensor, variables_module.Variable)):
-                while output.op.type == 'Identity':
-                    output = output.op.inputs[0]
-                if output.op.type == 'Softmax':
-                    # When softmax activation function is used for output operation, we
-                    # use logits from the softmax function directly to compute loss in order
-                    # to prevent collapsing zero when training.
-                    # See b/117284466
-                    assert len(output.op.inputs) == 1
-                    output = output.op.inputs[0]
-                    return nn.softmax_cross_entropy_with_logits_v2(labels=target, logits=output, axis=axis)
-            # scale preds so that the class probas of each sample sum to 1
-            output = output / math_ops.reduce_sum(output, axis, True)
-            # Compute cross entropy from probabilities.
-            epsilon_ = _constant_to_tensor(epsilon(), output.dtype.base_dtype)
-            output = clip_ops.clip_by_value(output, epsilon_, 1. - epsilon_)
-            return -math_ops.reduce_sum(target * math_ops.log(output)*weights, axis)
-
-        def weighted_categorical_crossentropy(y_true, y_pred, from_logits=False, label_smoothing=0):
-            y_pred = ops.convert_to_tensor_v2(y_pred)
-            y_true = math_ops.cast(y_true, y_pred.dtype)
-            label_smoothing = ops.convert_to_tensor_v2(label_smoothing, dtype=K.floatx())
-
-            def _smooth_labels():
-                num_classes = math_ops.cast(array_ops.shape(y_true)[1], y_pred.dtype)
-                return y_true * (1.0 - label_smoothing) + (label_smoothing / num_classes)
-
-            y_true = smart_cond.smart_cond(label_smoothing, _smooth_labels, lambda: y_true)
-
-            return weighted_keras_categorical_crossentropy(y_true, y_pred, from_logits=from_logits)
-
-        super(WeightedCategoricalCrossentropy, self).__init__(weighted_categorical_crossentropy, name=name, reduction=reduction,
-                                                              from_logits=from_logits, label_smoothing=label_smoothing)
 
 
 def categorical_crossentropy(y_true, y_pred, from_logits=False, label_smoothing=0):
